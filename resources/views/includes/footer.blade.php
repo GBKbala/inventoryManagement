6219ae6302cb2ca93bfaefb0c4867e9a68fb1781
GBKbala <balakannangbk01@gmail.com>
2024-08-15 10:26:08 +0530

    added assets folder

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,11 @@
+<script src="assets/js/jquery-3.7.1.min.js" type="8296422112d774d8b527ee40-text/javascript"></script>
+<script src="assets/js/feather.min.js" type="8296422112d774d8b527ee40-text/javascript"></script>
+<script src="assets/js/jquery.slimscroll.min.js" type="8296422112d774d8b527ee40-text/javascript"></script>
+<script src="assets/js/bootstrap.bundle.min.js" type="8296422112d774d8b527ee40-text/javascript"></script>
+<script src="assets/plugins/apexchart/apexcharts.min.js" type="8296422112d774d8b527ee40-text/javascript"></script>
+<script src="assets/plugins/apexchart/chart-data.js" type="8296422112d774d8b527ee40-text/javascript"></script>
+<script src="assets/plugins/sweetalert/sweetalert2.all.min.js" type="8296422112d774d8b527ee40-text/javascript"></script>
+<script src="assets/plugins/sweetalert/sweetalerts.min.js" type="8296422112d774d8b527ee40-text/javascript"></script>
+<script src="assets/js/theme-script.js" type="8296422112d774d8b527ee40-text/javascript"></script>
+<script src="assets/js/script.js" type="8296422112d774d8b527ee40-text/javascript"></script>
+<script src="https://dreamspos.dreamstechnologies.com/cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js" data-cf-settings="8296422112d774d8b527ee40-|49" defer></script>
---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,14 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+    <title>Inventory Management</title>
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/bootstrap-datetimepicker.min.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet" href="assets/plugins/select2/css/select2.min.css">
+    <link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css">
+    <link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
