83e653c1607d6c1cc3d2d874deaa2907b07ec52e
GBKbala <balakannangbk01@gmail.com>
2024-08-16 12:09:18 +0530

    user avatar added

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -13,8 +13,8 @@
         <!-- User -->
         <li class="nav-item navbar-dropdown dropdown-user dropdown">
             <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
-                <div class="avatar avatar-online">
-                    <img src="../../assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle">
+                <div class="avatar">
+                    <img src="{{ asset('assets/img/avatars/user.png') }}" alt class="w-px-40 h-auto rounded-circle">
                 </div>
             </a>
             <ul class="dropdown-menu dropdown-menu-end">
@@ -22,8 +22,8 @@
                     <a class="dropdown-item" href="pages-account-settings-account.html">
                     <div class="d-flex">
                         <div class="flex-shrink-0 me-3">
-                            <div class="avatar avatar-online">
-                                <img src="../../assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle">
+                            <div class="avatar">
+                                <img src="{{ asset('assets/img/avatars/user.png') }}" alt class="w-px-40 h-auto rounded-circle">
                             </div>
                         </div>
                         @if(auth()->user())
